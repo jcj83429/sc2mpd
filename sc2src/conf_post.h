627fee4f923e67cdcdc1fd28f9b219fb0f8e3eda
Jean-Francois Dockes <jf@dockes.org>
2016-07-14 10:14:54 +0200

    get rid of tr1 defs

--- sc2src/conf_post.h
+++ sc2src/conf_post.h
@@ -32,36 +32,3 @@ static const char *SC2SRC_PACKAGE_VERSION = PACKAGE_VERSION;
 #undef PACKAGE_TARNAME
 #undef PACKAGE_URL
 #undef PACKAGE_VERSION
-
-#define HAVE_CXX0X_UNORDERED
-#define HAVE_SHARED_PTR_STD
-// upmpdcli: requires c++0x but we still need the defs for c++
-// versions below because of execmd et al.
-
-#ifdef  HAVE_CXX0X_UNORDERED
-#  define UNORDERED_MAP_INCLUDE <unordered_map>
-#  define UNORDERED_SET_INCLUDE <unordered_set>
-#  define STD_UNORDERED_MAP std::unordered_map
-#  define STD_UNORDERED_SET std::unordered_set
-#elif defined(HAVE_TR1_UNORDERED)
-#  define UNORDERED_MAP_INCLUDE <tr1/unordered_map>
-#  define UNORDERED_SET_INCLUDE <tr1/unordered_set>
-#  define STD_UNORDERED_MAP std::tr1::unordered_map
-#  define STD_UNORDERED_SET std::tr1::unordered_set
-#else
-#  define UNORDERED_MAP_INCLUDE <map>
-#  define UNORDERED_SET_INCLUDE <set>
-#  define STD_UNORDERED_MAP std::map
-#  define STD_UNORDERED_SET std::set
-#endif
-
-#ifdef HAVE_SHARED_PTR_STD
-#  define MEMORY_INCLUDE <memory>
-#  define STD_SHARED_PTR    std::shared_ptr
-#elif defined(HAVE_SHARED_PTR_TR1)
-#  define MEMORY_INCLUDE <tr1/memory>
-#  define STD_SHARED_PTR    std::tr1::shared_ptr
-#else
-#  define MEMORY_INCLUDE "refcntr.h"
-#  define STD_SHARED_PTR    RefCntr
-#endif
